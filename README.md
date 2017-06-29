361689422b192836cc4442cb11cb3ece5bcbad67
yuzhongbin <yuzhongbin@gmail.com>
2017-06-29 23:36:03 +0800

    添加readme

---
+++ README.md
@@ -0,0 +1,3 @@
+www.poj.org练习示例
+
+环境依赖: gcc, cmake, gcov/lcov
---
+++ src/poj/1061/forg_1061.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+typedef long int64_t;
+#define MAX_POSITION  2000000000
+#define MAX_STEP      2000000000
+#define MAX_CIRCLE    2100000000
+
+class forg
+{
+public:
+  forg(int64_t step_len, int64_t position, int64_t circle)
+    : m_jump_count(0), m_step_len(step_len)
+    , m_position(position), m_circle(circle)
+    { }
+
+  int64_t jump()
+  { 
+    ++m_jump_count;
+
+    m_position += m_step_len;
+    if (m_position >= m_circle)
+      m_position = m_position % m_circle;
+
+    return m_position;
+  }
+
+  int64_t position() { return m_position; }
+
+  int64_t jump_count() { return m_jump_count; }
+
+private:
+  int64_t m_jump_count;
+  int64_t m_step_len;
+  int64_t m_position;   
+  int64_t m_circle;
+};
+
+int main()
+{
+  int64_t position_x = 0;
+  int64_t position_y = 0;
+  int64_t step_x = 0;
+  int64_t setp_y = 0;
+  int64_t line = 0;
+
+  std::cin >> position_x >> position_y >> step_x >> setp_y >> line;
+
+  if (position_x == position_y || position_x >= MAX_POSITION || position_y >= MAX_POSITION
+    || step_x >= MAX_STEP || step_x <= 0 || setp_y >= MAX_STEP || setp_y <= 0
+    || line >= MAX_CIRCLE || line <= 0) {
+    std::cout << "Impossible";
+    return 0;
+  }
+
+  forg x(position_x, step_x, line);
+  forg y(position_y, setp_y, line);
+
+  while(1) {
+    x.jump();
+    y.jump();
+
+    if (x.position() == y.position())
+      break;
+  }
+
+  std::cout << x.jump_count();
+
+  return 0;
+}
